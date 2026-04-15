1dd9ba573a95d2f943024ed55918c968c7d6e6ba
Alexander Liapustin <xanderkag@users.noreply.github.com>
2026-04-15 19:54:33 +0300

    chore(release) [v3.22.0]: bump version to force cache invalidation

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during build. Do not edit manually.
-export const APP_VERSION = '3.21.2';
-export const BUILD_TIME = '15.04.2026, 01:49';
+export const APP_VERSION = '3.22.0';
+export const BUILD_TIME = '15.04.2026, 19:47';
