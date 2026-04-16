ad22252035263f6be44556fd0c2aedf226858c95
Alexander Liapustin <xanderkag@users.noreply.github.com>
2026-04-17 00:51:10 +0300

    chore: Bump version to v3.22.20

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during build. Do not edit manually.
-export const APP_VERSION = '3.22.19';
-export const BUILD_TIME = '16.04.2026, 23:57';
+export const APP_VERSION = '3.22.20';
+export const BUILD_TIME = '17.04.2026, 00:50';
